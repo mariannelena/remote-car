772ec8eec49f039940d442c4ceced2a55f1dd86c
mariannelena <73900073+mariannelena@users.noreply.github.com>
2021-10-09 11:41:08 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
 radio.onReceivedNumber(function (receivedNumber) {
     Radio = receivedNumber
-    Kitronik_Move_Motor.turnRadius(Kitronik_Move_Motor.TurnRadii.Standard)
     Kitronik_Move_Motor.stop()
     if (Radio == 1) {
+        moveMotorZIP.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Green))
+        moveMotorZIP.show()
         basic.showLeds(`
             . . # . .
             . . . # .
@@ -10,9 +11,11 @@ radio.onReceivedNumber(function (receivedNumber) {
             . . . # .
             . . # . .
             `)
-        Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 30)
+        Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Left, 0)
         basic.pause(500)
     } else if (Radio == 2) {
+        moveMotorZIP.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Orange))
+        moveMotorZIP.show()
         basic.showLeds(`
             . . # . .
             . # . . .
@@ -20,9 +23,11 @@ radio.onReceivedNumber(function (receivedNumber) {
             . # . . .
             . . # . .
             `)
-        Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 30)
+        Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Right, 0)
         basic.pause(500)
     } else if (Radio == 3) {
+        moveMotorZIP.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Blue))
+        moveMotorZIP.show()
         basic.showLeds(`
             . . # . .
             . . # . .
@@ -33,6 +38,8 @@ radio.onReceivedNumber(function (receivedNumber) {
         Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 35)
         basic.pause(500)
     } else if (Radio == 4) {
+        moveMotorZIP.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Purple))
+        moveMotorZIP.show()
         basic.showLeds(`
             . . # . .
             . # # # .
@@ -42,6 +49,17 @@ radio.onReceivedNumber(function (receivedNumber) {
             `)
         Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, 30)
         basic.pause(500)
+    } else if (Radio == 5) {
+        Kitronik_Move_Motor.stop()
+        moveMotorZIP.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red))
+        moveMotorZIP.show()
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
     }
 })
 input.onButtonPressed(Button.A, function () {
@@ -55,4 +73,16 @@ input.onButtonPressed(Button.A, function () {
     Kitronik_Move_Motor.stop()
 })
 let Radio = 0
+let moveMotorZIP: Kitronik_Move_Motor.MoveMotorZIP = null
 radio.setGroup(1)
+Kitronik_Move_Motor.turnRadius(Kitronik_Move_Motor.TurnRadii.Standard)
+Kitronik_Move_Motor.motorBalance(Kitronik_Move_Motor.SpinDirections.Right, 7)
+moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
+moveMotorZIP.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.White))
+moveMotorZIP.show()
+Kitronik_Move_Motor.setUltrasonicUnits(Kitronik_Move_Motor.Units.Centimeters)
+basic.forever(function () {
+    if (Kitronik_Move_Motor.measure() < 5) {
+        Kitronik_Move_Motor.beepHorn()
+    }
+})
